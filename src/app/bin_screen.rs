c37b577b88c4f36a4ebda710ea32800e05ba6a9e
Aayush Kumar Sahu <aayushmau5@gmail.com>
2025-02-18 17:55:58 +0530

    opening files in bin

--- src/app/bin_screen.rs
+++ src/app/bin_screen.rs
@@ -7,11 +7,13 @@ use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style, Stylize},
     text::{Line, Span, Text},
-    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Paragraph},
+    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Padding, Paragraph},
     Frame,
 };
 use serde_json::{json, Value};
 use tokio::sync::mpsc::Sender;
+
+const ENDPOINT: &str = "http://localhost:4000";
 
 /// ## Events
 ///
@@ -24,6 +26,7 @@ pub struct BinScreen {
     bins: Vec<Bin>,
     list_state: ListState,
     selected: Option<Bin>,
+    selected_file: ListState,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
@@ -50,6 +53,7 @@ impl BinScreen {
             bins: Vec::new(),
             list_state: ListState::default(),
             selected: None,
+            selected_file: ListState::default(),
         };
         bin_screen.push_event(Some(json!({"action": "get-all"})));
         bin_screen
@@ -102,40 +106,111 @@ impl BinScreen {
     fn bin_widget(&mut self, area: Rect, f: &mut Frame) {
         let selected = self.selected.as_ref().unwrap();
 
-        let block = Block::new()
+        if !selected.files.is_empty() {
+            return self.bin_widget_with_files(area, f);
+        }
+
+        let layout_chunks = Layout::default()
+            .constraints([Constraint::Length(3), Constraint::Min(1)])
+            .split(area);
+
+        let top_block = Block::new()
             .border_type(BorderType::Rounded)
-            .borders(Borders::ALL)
+            .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
             .border_style(Style::default().fg(Color::Green))
             .style(Style::new().green())
             .title(format!("{} - Bin", selected.title));
 
         let formatted_date_time = selected.expire_at.format("%d/%m/%Y %I:%M %p").to_string();
+        let expire_text = Text::style(
+            format!("Expire at: {}\n\n", formatted_date_time).into(),
+            Style::default().fg(Color::Blue),
+        );
+        let expire_paragraph = Paragraph::new(expire_text).block(top_block);
+        f.render_widget(expire_paragraph, layout_chunks[0]);
 
-        let mut text = format!("Expire at: {}\n\n{}", formatted_date_time, selected.content);
+        let content_block = Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::LEFT | Borders::RIGHT | Borders::BOTTOM)
+            .border_style(Style::default().fg(Color::Green))
+            .style(Style::new().green());
+        let content_text = Text::styled(
+            format!("{}", selected.content),
+            Style::default().fg(Color::Magenta),
+        );
+        let content_paragraph = Paragraph::new(content_text).block(content_block);
+        f.render_widget(content_paragraph, layout_chunks[1]);
+    }
 
-        if selected.files.len() != 0 {
-            let mut files = String::new();
-            let _: Vec<_> = selected
-                .files
-                .iter()
-                .map(|file| {
-                    let formatted = format!("{}\n{}\n\n", file.name, file.type_name);
-                    files.push_str(&formatted.as_str());
-                })
-                .collect();
-            text.push_str(format!("\n\nFiles:\n\n{}", files).as_str());
-        }
+    fn bin_widget_with_files(&mut self, area: Rect, f: &mut Frame) {
+        let layout_chunks = Layout::default()
+            .constraints([
+                Constraint::Length(3),
+                Constraint::Min(1),
+                Constraint::Min(1),
+            ])
+            .split(area);
 
-        let text = Text::styled(text, Style::default().fg(Color::Magenta));
-        let content_paragraph = Paragraph::new(text).block(block);
+        let selected = self.selected.as_ref().unwrap();
 
-        f.render_widget(content_paragraph, area);
+        let top_block = Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
+            .border_style(Style::default().fg(Color::Green))
+            .style(Style::new().green())
+            .title(format!("{} - Bin", selected.title));
+
+        let formatted_date_time = selected.expire_at.format("%d/%m/%Y %I:%M %p").to_string();
+        let expire_text = Text::style(
+            format!("Expire at: {}\n\n", formatted_date_time).into(),
+            Style::default().fg(Color::Blue),
+        );
+        let expire_paragraph = Paragraph::new(expire_text).block(top_block);
+        f.render_widget(expire_paragraph, layout_chunks[0]);
+
+        let content_block = Block::new()
+            .borders(Borders::LEFT | Borders::RIGHT)
+            .border_style(Style::default().fg(Color::Green))
+            .style(Style::new().green());
+        let content_text = Text::styled(
+            format!("{}", selected.content),
+            Style::default().fg(Color::Magenta),
+        );
+        let content_paragraph = Paragraph::new(content_text).block(content_block);
+        f.render_widget(content_paragraph, layout_chunks[1]);
+
+        let file_block = Block::new()
+            .border_type(BorderType::Rounded)
+            .borders(Borders::LEFT | Borders::RIGHT | Borders::BOTTOM)
+            .border_style(Style::default().fg(Color::Green))
+            .style(Style::new().green())
+            .padding(Padding::top(1))
+            .title("Files:");
+        let files = selected.files.iter().map(|file| {
+            let formatted = Text::from(vec![
+                Line::from(file.name.as_str()),
+                Line::from(file.type_name.as_str()).style(Style::default().fg(Color::Blue)),
+                Line::from("\n"),
+            ]);
+            ListItem::new(formatted).style(Style::default().fg(Color::Cyan))
+        });
+        let list = List::new(files)
+            .highlight_style(Style::new().bg(Color::Green))
+            .style(Style::new().green())
+            .highlight_symbol("-> ")
+            .repeat_highlight_symbol(false)
+            .block(file_block);
+        f.render_stateful_widget(list, layout_chunks[2], &mut self.selected_file);
     }
 
     fn help_widget(&self, area: Rect, f: &mut Frame) {
         let mut help_message = "(q) to quit / (b) back to main menu / (n) to add a new bin / (d) to delete a bin / (e) to edit a bin / (enter) to see a bin";
         if !self.selected.is_none() {
-            help_message = "(q) to quit / (b) back to bin menu / (d) to delete this bin / (e) to edit this bin";
+            if self.selected_file.selected().is_none() {
+                help_message = "(q) to quit / (b) back to bin menu / (d) to delete this bin / (e) to edit this bin";
+            } else {
+                help_message = "(q) to quit / (b) back to bin menu / (o) open file / (d) to delete this bin / (e) to edit this bin";
+            }
         }
 
         let help_widget = Span::styled(help_message, Style::default().fg(Color::Blue));
@@ -156,36 +231,69 @@ impl BinScreen {
                 }
             }
             KeyCode::Char('h') | KeyCode::Left => {
+                if !self.selected.is_none() {
+                    return None;
+                }
                 self.select_none();
                 None
             }
             KeyCode::Char('j') | KeyCode::Down => {
+                if !self.selected.is_none() && !self.selected.as_ref().unwrap().files.is_empty() {
+                    self.select_next_file();
+                    return None;
+                }
                 self.select_next();
                 None
             }
             KeyCode::Char('k') | KeyCode::Up => {
+                if !self.selected.is_none() && !self.selected.as_ref().unwrap().files.is_empty() {
+                    self.select_previous_file();
+                    return None;
+                }
                 self.select_previous();
                 None
             }
             KeyCode::Char('g') | KeyCode::Home => {
+                if !self.selected.is_none() {
+                    return None;
+                }
                 self.select_first();
                 None
             }
             KeyCode::Char('G') | KeyCode::End => {
+                if !self.selected.is_none() {
+                    return None;
+                }
                 self.select_last();
+                None
+            }
+            KeyCode::Char('o') => {
+                if !self.selected.is_none() && !self.selected_file.selected().is_none() {
+                    let selected_index = self.selected_file.selected().unwrap();
+                    let selected_file = &self.selected.as_ref().unwrap().files[selected_index];
+
+                    match open::that(format!("{}{}", ENDPOINT, selected_file.access_path)) {
+                        Ok(()) => {}
+                        Err(e) => info!("Failed to open file: {e}"),
+                    }
+                }
                 None
             }
             KeyCode::Char('e') => {
                 if let Some(index) = self.list_state.selected() {
                     let selected = self.bins[index].clone();
                     self.selected = Some(selected);
+                    self.selected_file = ListState::default().with_selected(None);
                 }
                 None
             }
             KeyCode::Char('l') | KeyCode::Enter => {
-                if let Some(index) = self.list_state.selected() {
-                    let selected = self.bins[index].clone();
-                    self.selected = Some(selected);
+                if self.selected.is_none() {
+                    if let Some(index) = self.list_state.selected() {
+                        let selected = self.bins[index].clone();
+                        self.selected = Some(selected);
+                        self.selected_file = ListState::default().with_selected(None);
+                    }
                 }
                 None
             }
@@ -239,5 +347,13 @@ impl BinScreen {
 
     fn select_last(&mut self) {
         self.list_state.select_last();
+    }
+
+    fn select_previous_file(&mut self) {
+        self.selected_file.select_previous();
+    }
+
+    fn select_next_file(&mut self) {
+        self.selected_file.select_next();
     }
 }
